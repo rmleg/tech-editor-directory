9175652788303013768cc35d5891f00896409523
Rachel Leggett <leggett.rachel@gmail.com>
2020-08-26 22:12:59 -0400

    Set up blogging with markdown. Next up, proof of concept of filtering on frontmatter?

---
+++ content/sample-post.md
@@ -0,0 +1,13 @@
+---
+slug: 'sample-post'
+title: Sample Post
+author: 'Rachel Leggett'
+---
+
+# This is my sample post
+
+This is some content in my sample post.
+
+## Thanks for looking!
+
+This is great.
--- src/routes/blog/[slug].json.js
+++ src/routes/blog/[slug].json.js
@@ -1,28 +1,47 @@
-import posts from './_posts.js';
+// import posts from "./_posts.js";
 
-const lookup = new Map();
-posts.forEach(post => {
-	lookup.set(post.slug, JSON.stringify(post));
-});
+import path from "path";
+import fs from "fs";
+import grayMatter from "gray-matter";
+import marked from "marked";
+
+const getPost = (fileName) =>
+  fs.readFileSync(path.resolve("content", `${fileName}.md`), "utf-8");
+
+/* const lookup = new Map();
+posts.forEach((post) => {
+  lookup.set(post.slug, JSON.stringify(post));
+}); */
 
 export function get(req, res, next) {
-	// the `slug` parameter is available because
-	// this file is called [slug].json.js
-	const { slug } = req.params;
+  // the `slug` parameter is available because
+  // this file is called [slug].json.js
+  const { slug } = req.params;
 
-	if (lookup.has(slug)) {
-		res.writeHead(200, {
-			'Content-Type': 'application/json'
-		});
+  // get the markdown text
+  const post = getPost(slug);
 
-		res.end(lookup.get(slug));
-	} else {
-		res.writeHead(404, {
-			'Content-Type': 'application/json'
-		});
+  // parse the md to get front matter
+  // and the content without escaping characters
+  const { data, content } = grayMatter(post);
 
-		res.end(JSON.stringify({
-			message: `Not found`
-		}));
-	}
+  const html = marked(content);
+
+  if (html) {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+
+    res.end(JSON.stringify({ html, ...data }));
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "application/json",
+    });
+
+    res.end(
+      JSON.stringify({
+        message: `Not found`,
+      })
+    );
+  }
 }
